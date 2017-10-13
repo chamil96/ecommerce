c87a04e624ab1a37b15f2fcd1afdcf2b27c3136e
Chris Hamilton <chrishamilton@Administrators-MacBook-Pro.local>
2017-10-13 11:58:34 -0400

    Project updates with comments

--- e-commerce 2k17/index.php
+++ e-commerce 2k17/index.php
@@ -8,6 +8,26 @@
             <img src="lib/img/homeimg/recordplayer.gif" alt="record player">
             <img src="lib/img/homeimg/vinyldisplay2.jpg" alt="display">
         </div><!-- fotorama -->
+        <div class="featured_content row">
+          <h2 id="feature_header">Featured Items</h2>
+        </div>
+        <div class="featured_content row">
+          <div class="featured_columns">
+            <h3>Sony USB Stereo Turntable, Black</h3>
+            <figure><img src="lib/img/recordplayers/sony1.jpg" alt="Sony"></figure>
+            <figcaption>$129.99</figcaption>
+          </div>
+          <div class="featured_columns">
+            <h3>Wooden Vinyl Storage Cube Shelf</h3>
+            <figure><img src="lib/img/displays/storagecubeshelf1.jpeg" alt="shelf"></figure>
+            <figcaption>$35.00</figcaption>
+          </div>
+          <div class="featured_columns">
+            <h3>What's Going On? (1971)</h3>
+            <figure><img src=lib/img/records/marvingaye1.jpg alt="Marvin"></figure>
+            <figcaption>$20.00</figcaption>
+          </div>
+        </div>
       </div><!-- main_content -->
 <?php
   include ("lib/inc/footer.php");
